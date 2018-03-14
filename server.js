0866fae414f5e702c844a3c3ff6d978d3b246508
Scott Forman <sjforman@gmail.com>
2018-03-14 11:30:21 -0700

    Adds server, Procfile, and tachyons.

---
+++ server.js
@@ -0,0 +1,32 @@
+ //server.js
+'use strict'
+
+var express = require('express');
+var cors = require('cors');
+var path = require('path');
+
+var app = express();
+var router = express.Router();
+var port = process.env.REACT_APP_API_PORT || 3001;
+var corsOption = {
+  origin: true,
+  moethods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+}
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors(corsOption));
+
+router.route('/')
+  .get(function(req, res) {
+    res.json('hi');
+  })
+
+//Use router configuration at /api
+app.use('/api', router);
+
+//start server and listen for requests
+app.listen(port, function() {
+  console.log(`api running on port ${port}`);
+});
