710f500cfe399a3011719280b93e07870fa77974
Scott Forman <sjforman@gmail.com>
2018-03-15 17:19:02 -0700

    Removes some React boilerplate.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App tc mt6">
+        <img src={logo} className="App-logo w5" alt="logo" />
+        <h1 className="title">Vote Forward</h1>
       </div>
     );
   }
