501e4066c00b8e5757758e84579506fa9a9b531d
Scott Forman <sjforman@gmail.com>
2018-03-20 14:55:39 -0700

    Shows addresses of adopted voters

--- src/VoterList.js
+++ src/VoterList.js
@@ -7,16 +7,18 @@ export class VoterList extends Component {
   render() {
     return (
       <div>
-        <h2 className="title tc">Your adopted voters</h2>
+        <h2 className="title tc">Your Voters</h2>
         <ul className="list pl0 mt0 measure center">
         {this.props.voters.map(voter =>
           <li className="flex items-center lh-copy pa3 ph0-l bb b--black-10" key={voter.id}>
             <Avatar size={50} round={true} name={voter.first_name + ' ' + voter.last_name}/>
             <div className="pl3 flex-auto">
-              <span className="f6 db black-70">{voter.first_name} {voter.last_name}</span>
-            </div>
-            <div>
-              <a href="address:" className="f6 link blue hover-dark-gray">{voter.state}</a>
+              <span className="f6 db black-70">{voter.first_name} {voter.last_name}
+                <br/>
+                {voter.address}
+                <br/>
+                {voter.city}, {voter.state} {voter.zip}
+              </span>
             </div>
           </li>
         )}
