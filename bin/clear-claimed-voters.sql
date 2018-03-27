d2729612ed9fdc34604a9ea3f68b473347202ac4
Scott Forman <sjforman@gmail.com>
2018-03-27 15:44:23 -0700

    Fixes clear-db script.

--- bin/clear-claimed-voters.sql
+++ bin/clear-claimed-voters.sql
@@ -1,4 +1,5 @@
-update voters set plea_letter_sent_timestamp=null where plea_letter_sent_timestamp is not null;
-update voters set adoption_timestamp=null where adoption_timestamp is not null;
-update voters set plea_letter_url='' where plea_letter_url is not null;
+update voters set confirmed_sent_at=null where confirmed_sent_at is not null;
+update voters set adopted_at=null where adopted_at is not null;
+update voters set plea_letter_url=null where plea_letter_url is not null;
 update voters set adopter_user_id=null where adopter_user_id is not null;
+update voters set pledge_made_at=null where pledge_made_at is not null;
