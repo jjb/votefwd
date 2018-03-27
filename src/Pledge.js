1f4e04d71f0c87bc74ed2d98161a410edb4f6d96
Scott Forman <sjforman@gmail.com>
2018-03-27 15:28:55 -0700

    Adds beginning of voter pledge form.

--- src/Pledge.js
+++ src/Pledge.js
@@ -3,19 +3,45 @@
 import React, { Component } from 'react';
 import { Header } from './Header';
 
-class Pledge extends Component {
+class PledgeForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {value: ''};
 
-    this.state = { pledgeStatus: false }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A code was submitted: ' + this.state.value);
+    event.preventDefault();
   }
 
   render() {
     return (
+      <div className="tc">
+        <form className="center db ma2" onSubmit={this.handleSubmit}>
+          <label>
+            Enter your voter pledge code here:
+            <input className="center db ma2" type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input className="center db ma2" type="submit" value="Pledge to be a voter." />
+        </form>
+      </div>
+    );
+  }
+}
+
+class Pledge extends Component {
+  render() {
+    return (
       <div>
         <Header />
-        <p className="measure">Hi</p>
-        <p>{this.state.pledgeStatus}</p>
+        <PledgeForm />
       </div>
     );
   }
