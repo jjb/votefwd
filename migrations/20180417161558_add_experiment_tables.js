29e8be9de2006fef79879431fc464597e34b1c41
Scott Forman <sjforman@gmail.com>
2018-04-19 15:11:01 -0700

    Simplifies experimental setup; adds randomization and targeting for Ohio 12th

--- migrations/20180417161558_add_experiment_tables.js
+++ migrations/20180417161558_add_experiment_tables.js
@@ -2,28 +2,21 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema
-    .createTable('experiments', function (table) {
+    .createTable('experiment', function (table) {
       table.increments();
-      table.string('name', 16).unique().notNullable();
-      table.string('randomization_query');
-      table.date('randomization_date');
+      table.string('name', 32).unique().notNullable();
+      table.string('description', 128);
     })
-    .createTable('experiment_variant', function (table) {
+    .createTable('experiment_voter', function (table) {
       table.increments();
-      table.string('name', 16).unique().notNullable();
-      table.integer('experiment_id').unsigned().references('id').inTable('experiments').notNullable();
-    })
-    .createTable('voter_experiment_variant', function (table) {
-      table.increments();
-      table.string('voter_registration_id').references('registration_id').inTable('voters').notNullable();
-      table.integer('experiment_id').references('id').inTable('experiments').notNullable();
-      table.integer('variant_id').references('id').inTable('experiment_variant').notNullable();
+      table.string('voter_id').notNullable();
+      table.integer('experiment_id').references('id').inTable('experiment');
+      table.enu('cohort', ['TEST', 'CONTROL']);
     });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema
-    .dropTableIfExists('voter_experiment_variant')
-    .dropTableIfExists('experiment_variant')
-    .dropTableIfExists('experiments');
+    .dropTableIfExists('experiment_voter')
+    .dropTableIfExists('experiment');
 };
