5d3593bc7a1e8f88c81a317a1a26909e25601c5e
Scott Forman <sjforman@gmail.com>
2018-04-20 14:00:49 -0700

    Updates ohio targeting script with notices to console and Andy suggestions.

--- bin/ohio-targeting.sql
+++ bin/ohio-targeting.sql
@@ -1,14 +1,15 @@
 DO $$
 
-DECLARE 
+DECLARE
   experimentid integer;
+  votercount integer := 1000;
 
 BEGIN
 
   -- DESCRIBE THE EXPERIMENT
 
   INSERT INTO experiment VALUES
-    (DEFAULT, 'OHIO_SPECIAL', 'Special election, test of timing and letter variants, no reply cards.') 
+    (DEFAULT, 'OHIO_SPECIAL', 'Special election, test of timing and letter variants, no reply cards.')
     RETURNING id INTO experimentid;
 
   -- INSERT IDs OF ALL POSSIBLE TARGETS INTO EXPERIMENT TRACKING TABLE
@@ -16,13 +17,15 @@ BEGIN
   INSERT INTO experiment_voter (voter_id, experiment_id)
   SELECT sos_voterid, experimentid
   FROM ohio_12_wide
-  WHERE party_affiliation='D' 
-    AND general_11_08_2016='X' 
-    AND primary_05_02_2017!='X' 
-    AND primary_09_12_2017!='X' 
-    AND general_11_07_2017!='X' 
-    AND voter_status='ACTIVE' 
+  WHERE party_affiliation='D'
+    AND general_11_08_2016='X'
+    AND primary_05_02_2017!='X'
+    AND primary_09_12_2017!='X'
+    AND general_11_07_2017!='X'
+    AND voter_status='ACTIVE'
     AND mailing_address1='';
+
+  RAISE NOTICE 'Registered experiment as id: %', experimentid;
 
   -- GRAB SOME VOTERS AT RANDOM AND UPDATE THEIR DESIGNATIONS
 
@@ -32,28 +35,31 @@ BEGIN
   (
     SELECT voter_id FROM experiment_voter
     WHERE experiment_id = experimentid
+    AND cohort is null
     ORDER BY RANDOM()
-    LIMIT 100 
+    LIMIT votercount
   );
+
+  RAISE NOTICE 'Assigned % voters to TEST group.', votercount;
 
   -- POPULATE SELECTED TEST SUBJECTS INTO VOTERS TO MAKE ADOPTABLE
 
-  INSERT INTO voters 
+  INSERT INTO voters
     ( registration_id,
-      first_name, 
-      middle_name, 
-      last_name, 
+      first_name,
+      middle_name,
+      last_name,
       suffix,
-      address, 
-      city, 
+      address,
+      city,
       state,
-      zip, 
+      zip,
       date_of_birth
     )
   SELECT sos_voterid,
-    first_name, 
-    middle_name, 
-    last_name, 
+    first_name,
+    middle_name,
+    last_name,
     suffix,
     residential_address1 || ' ' || residential_secondary_addr,
     residential_city,
@@ -64,5 +70,7 @@ BEGIN
   JOIN experiment_voter
   ON experiment_voter.voter_id = ohio_12_wide.sos_voterid
   WHERE experiment_voter.cohort = 'TEST';
+
+  RAISE NOTICE 'Populated voters table with new TEST voters.';
 
 END$$;
