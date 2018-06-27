5367a4a695a54ab5984f4994b3eabd017e2c2e33
andy timmons <andrewjtimmons@gmail.com>
2018-06-26 21:23:39 -0700

    add trash that doesnt work, but shows a cool error if you disable CORS on localhost

--- src/Auth.js
+++ src/Auth.js
@@ -13,17 +13,38 @@ export default class Auth {
     responseType: 'token id_token',
     scope: 'openid profile email'
   });
-  
+
   constructor() {
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.handleAuthentication = this.handleAuthentication.bind(this);
     this.isAuthenticated = this.isAuthenticated.bind(this);
-  } 
+  }
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
+
+        //This is trash and doesnt work, but is kinda part way there https://auth0.com/docs/api/authentication#implicit-grant
+        function reqListener () {
+          console.log(this.responseText);
+        }
+        var api_auth = new XMLHttpRequest();
+        api_auth.addEventListener("load", reqListener);
+        api_auth.open("GET",
+              'https://votefwd.auth0.com/authorize?' +
+              'audience=https://votefwd.auth0.com/api&' +
+              'scope=read&' +
+              'response_type=token&' +
+              'client_id=' + process.env.REACT_APP_AUTH0_CLIENTID +
+              'redirectUri=' + process.env.REACT_APP_URL + '/callback' + '&' +
+              'prompt=none'
+              // redirect_uri=http://localhost:3000&
+              // state=STATE& // probably should have this post mvp
+              // nonce=NONCE // this too
+        );
+        api_auth.send();
+
         this.setSession(authResult);
         this.persistUser(authResult, () => {
           history.replace('/dashboard');
@@ -54,7 +75,7 @@ export default class Auth {
       localStorage.setItem('picture_url', profile.picture);
     })
     history.replace('/');
-  }    
+  }
 
   logout() {
     // Clear Access Token and ID Token from local storage
@@ -68,7 +89,7 @@ export default class Auth {
   }
 
   isAuthenticated() {
-    // Check whether the current time is past the 
+    // Check whether the current time is past the
     // Access Token's expiry time
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
