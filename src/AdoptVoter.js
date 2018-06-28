08c42814d5cfbe7e623114ff728c87d29b3a411b
Michael Dunkle <michael.dunkle@gmail.com>
2018-06-28 16:42:50 -0600

    Basic dashboard styling

--- src/AdoptVoter.js
+++ src/AdoptVoter.js
@@ -35,7 +35,7 @@ export class AdoptVoter extends Component {
     let content;
     if (this.state.adopting) {
       content = (
-        <p className="f4 red">Assigning voters to you...this may take a minute.</p>
+        <div className="alert alert-warning mt-3 mb-0" role="alert">Assigning voters to you...this may take a minute.</div>
       )
     }
     return (
@@ -44,12 +44,10 @@ export class AdoptVoter extends Component {
         <hr className="my-4" />
         <p>Click the button for the number of voters you’d like to adopt. We’ll generate letters for each one that you can download and print.</p>
         <p>Voters you adopt are yours exclusively. They won’t be assigned to anyone else. By adopting a voter, you’re committing to sending a letter. We’re counting on you!</p>
-        <div className="btn-group mb-3" role="group">
-          <button onClick={() => this.adoptVoter(1)} className="btn btn-primary btn-lg">1</button>
-          <button onClick={() => this.adoptVoter(5)} className="btn btn-primary btn-lg">5</button>
-          <button onClick={() => this.adoptVoter(15)} className="btn btn-primary btn-lg">15</button>
-          {content}
-        </div>
+        <button onClick={() => this.adoptVoter(1)} className="btn btn-primary btn-lg">1</button>
+        <button onClick={() => this.adoptVoter(5)} className="btn btn-primary btn-lg">5</button>
+        <button onClick={() => this.adoptVoter(15)} className="btn btn-primary btn-lg">15</button>
+        {content}
       </div>
     )
   }
--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,14 @@
 
 import React, { Component } from 'react';
 import { Login } from './Login';
-import logo from './logo.svg';
+import logo from './images/vote-forward-logo.svg';
 
 export class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
+      <nav className="navbar navbar-light bg-light mb-4">
         <a className="navbar-brand" href="/">
-          <img src={logo} className="d-inline-block align-top" alt="logo" />
-          Vote Forward
+          <img src={logo} className="d-inline-block align-top logo" alt="Vote Forward" />
         </a>
         { this.props.auth.isAuthenticated() && <Login auth={this.props.auth} />}
       </nav>
