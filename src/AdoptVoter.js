ed6377ef0915baf2f82c565e93604c01f6d1a587
andy timmons <andrewjtimmons@gmail.com>
2018-06-27 20:20:16 -0700

    handle variable number of voters in front end

--- src/AdoptVoter.js
+++ src/AdoptVoter.js
@@ -12,16 +12,19 @@ export class AdoptVoter extends Component {
   }
 
   adoptVoter() {
+    var numVoters = 1; //TODO: figure out how to get picker on dashboard.js
     this.setState({adopting: true});
     let user_id = localStorage.getItem('user_id');
     axios({
       method: 'POST',
       url: `${process.env.REACT_APP_API_URL}/voter/adopt-random`,
-      data: { adopterId: user_id }
+      data: {
+          adopterId: user_id,
+          numVoters: numVoters
+        }
       })
       .then(res => {
-        let encodedUri = encodeURI(res.data.pdfUrl);
-        this.props.handleAdoptedVoter(res.data.voter, encodedUri);
+        this.props.handleAdoptedVoter(res.data.voters);
         this.setState({adopting: false});
       })
       .catch(err => {
@@ -32,7 +35,7 @@ export class AdoptVoter extends Component {
   render() {
     let content;
     if (this.state.adopting) {
-      content = ( 
+      content = (
         <p className="f4 red">Finding a voter for you to adopt...</p>
       )
     }
