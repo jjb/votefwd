56ae2554d8ef909fcef0a0c9d546ce0ac01a178b
Michael Dunkle <michael.dunkle@gmail.com>
2018-06-28 18:32:21 -0600

    Changes nav background

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ import logo from './images/vote-forward-logo.svg';
 export class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-light bg-light mb-4">
+      <nav className="navbar navbar-light mb-4">
         <a className="navbar-brand" href="/">
           <img src={logo} className="d-inline-block align-top logo" alt="Vote Forward" />
         </a>
