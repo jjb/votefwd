dff9328b7b6cf916babdd9769e6d174de96615a0
Garrett Miller <gmiller@slack-corp.com>
2018-07-05 09:47:34 -0700

    Starting polish of design

--- src/Home.js
+++ src/Home.js
@@ -7,15 +7,15 @@ import { Login } from './Login';
 class Welcome extends Component {
   render() {
     return (
-      <div className="justify-items-center">
+      <div className="home--action-container col col-lg-6 p-4 h-50">
         { this.props.auth.isAuthenticated() ?
           (
-            <div className="text-center pt-5 pb-5">
+            <div className="">
               <a className="btn-lg" href="/dashboard">You’re signed in. Click here to send letters!</a>
             </div>
           ) :
           (
-            <div className="text-center pt-5 pb-5">
+            <div className="text-center p-4">
               <Login auth={this.props.auth} buttonText="Sign Up Or Log In To Send Letters" />
             </div>
           )
@@ -28,12 +28,14 @@ class Welcome extends Component {
 class Home extends Component {
   render() {
     return (
-      <div className="sans-serif tc">
+      <div className="h-100">
         <Header auth={this.props.auth} />
-        <Welcome auth={this.props.auth} />
-        <div className="text-center">
-          <p>Received a letter? <a className="link" href="/pledge">Click here to pledge to vote</a>.</p>
-          <p>Not sure what this is? <a className="link" target="_blank" rel="noopener noreferrer" href="https://votefwd.org">Click here to learn more</a>.</p>
+        <div className="row h-100">
+          <Welcome auth={this.props.auth} />
+          <div className="col col-lg-6 p-4 bg-lightblue h-50">
+            <p>Received a letter? <a className="link" href="/pledge">Click here to pledge to vote</a>.</p>
+            <p>Not sure what this is? <a className="link" target="_blank" rel="noopener noreferrer" href="https://votefwd.org">Click here to learn more</a>.</p>
+          </div>
         </div>
       </div>
     );
