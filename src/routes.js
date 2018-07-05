cbe6ba8a2c0d683854b6ca95283c0089d48c0394
Garrett Miller <gmiller@slack-corp.com>
2018-07-05 09:47:34 -0700

    Starting polish of design

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,7 @@ const handleAuthentication = ({location}) => {
 export const makeMainRoutes = () => {
   return (
       <Router history={history}>
-        <div>
+        <div className="h-100">
           <Route exact path="/" render={(props) => <Home auth={auth} {...props} />} />
           <Route exact path="/callback" render={(props) => {
             handleAuthentication(props);
