e50430fc2ed444a745bffa960d3786da94fc957e
Garrett Miller <gmiller@slack-corp.com>
2018-07-06 15:15:06 -0700

    cleanup

--- src/Footer.js
+++ src/Footer.js
@@ -15,17 +15,17 @@ export class Footer extends Component {
 					<div className="col-lg-6 h-100 text-center text-lg-right my-auto">
 					<ul className="list-inline mb-0">
 						<li className="list-inline-item mr-3">
-						<a href="https://fb.me/votefwd" target="_blank">
+						<a href="https://fb.me/votefwd" target="_blank" rel="noopener noreferrer">
 							<i className="text-lightblue fa fa-facebook fa-2x fa-fw"></i>
 						</a>
 						</li>
 						<li className="list-inline-item mr-3">
-						<a href="https://twitter.com/votefwd" target="_blank">
+						<a href="https://twitter.com/votefwd" target="_blank" rel="noopener noreferrer">
 							<i className="text-lightblue fa fa-twitter fa-2x fa-fw"></i>
 						</a>
 						</li>
 						<li className="list-inline-item">
-						<a href="https://www.instagram.com/votefwd/" target="_blank">
+						<a href="https://www.instagram.com/votefwd/" target="_blank" rel="noopener noreferrer">
 							<i className="text-lightblue fa fa-instagram fa-2x fa-fw"></i>
 						</a>
 						</li>
