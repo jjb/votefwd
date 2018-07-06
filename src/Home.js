0664927c2ede25f100fd584712020df4b1aa3ba5
Garrett Miller <gmiller@slack-corp.com>
2018-07-06 15:07:15 -0700

    Adding secret login page

--- src/Home.js
+++ src/Home.js
@@ -7,26 +7,6 @@ import { CallToAction } from './CallToAction';
 import { Footer } from './Footer';
 import { Login } from './Login';
 
-class Welcome extends Component {
-  render() {
-    return (
-      <div className="home--action-container col col-lg-6 p-4 h-50">
-        { this.props.auth.isAuthenticated() ?
-          (
-            <div className="">
-              <a className="btn-lg" href="/dashboard">You’re signed in. Click here to send letters!</a>
-            </div>
-          ) :
-          (
-            <div className="text-center pt-5 pb-5">
-              <Login auth={this.props.auth} buttonText="Sign Up Or Log In to send letters" />
-            </div>
-          )
-        }
-      </div>
-    );
-  }
-}
 class Home extends Component {
     render() {
       return (
@@ -35,12 +15,6 @@ class Home extends Component {
           <LandingShowcase />
           <CallToAction />
           <Footer />
-          <div className="row h-100">
-            <Welcome auth={this.props.auth} />
-            <div className="col col-lg-6 p-4 bg-lightblue h-50">
-              <p>Not sure what this is? <a className="link" target="_blank" rel="noopener noreferrer" href="https://votefwd.org">Click here to learn more</a>.</p>
-            </div>
-          </div>
         </React.Fragment>
       );
     }
