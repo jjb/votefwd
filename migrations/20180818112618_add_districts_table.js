7feb07d94fa1d7511b01aa300846ff3875c00700
Scott Forman <sjforman@gmail.com>
2018-08-18 11:57:27 -0700

    Updates district schema to have unique district id.

--- migrations/20180818112618_add_districts_table.js
+++ migrations/20180818112618_add_districts_table.js
@@ -3,8 +3,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('districts', function (table) {
     table.increments()
+    table.string('district_id', 16).unique();
     table.string('state', 16);
-    table.string('district', 16);
+    table.string('state_abbr', 16);
+    table.string('district_num', 16);
     table.string('description', 512);
     table.float('lat');
     table.float('long');
