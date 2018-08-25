6f0ecab2321318f8ddd2badc94048e2465d950fb
Michael Dunkle <michael.dunkle@gmail.com>
2018-08-25 10:35:02 -0600

    minimal styling of homepage districts

--- src/LandingDistricts.js
+++ src/LandingDistricts.js
@@ -4,11 +4,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class DistrictItem extends Component {
-	render() {
-		return (
-		  <h1>{this.props.district.district_id}</h1>
-		);
-	}
+  render() {
+    return (
+      <div className="col-3 display-4 text-primary">
+        {this.props.district.district_id}
+      </div>
+    );
+  }
 }
 
 export class LandingDistricts extends Component {
@@ -16,21 +18,23 @@ export class LandingDistricts extends Component {
     super(props);
 
     this.getDistricts = this.getDistricts.bind(this);
-    this.state = { districts:  [] };
+    this.state = { districts: [] };
   }
 
   getDistricts() {
     axios({
-      headers: {Authorization: 'Bearer '.concat(localStorage.getItem('access_token'))},
+      headers: {
+        Authorization: 'Bearer '.concat(localStorage.getItem('access_token'))
+      },
       method: 'GET',
       url: `${process.env.REACT_APP_API_URL}/get-districts`
-      })
+    })
       .then(res => {
-        this.setState({districts: res.data});
+        this.setState({ districts: res.data });
       })
       .catch(err => {
         console.error(err);
-    })
+      });
   }
 
   componentWillMount() {
@@ -39,13 +43,15 @@ export class LandingDistricts extends Component {
 
   render() {
     return (
-      <div>
-        {this.state.districts.map(district => 
-          <DistrictItem
-            key={district.id}
-            district={district}
-          />
-        )}
+      <div className="container">
+        <div className="row pt-5 text-center">
+          <div className="col-12">
+            <h3 className="mb-5">Available Districts</h3>
+          </div>
+          {this.state.districts.map(district => (
+            <DistrictItem key={district.id} district={district} />
+          ))}
+        </div>
       </div>
     );
   }
