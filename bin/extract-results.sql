c6bfc1801b8418c7d5093201d9de71216b282c9b
Scott Forman <sjforman@gmail.com>
2018-09-04 14:52:03 -0700

    Updates extract script and adds /results to .gitignore.

--- bin/extract-results.sql
+++ bin/extract-results.sql
@@ -1,34 +1,3 @@
-\copy (SELECT
-  voter_id as dwid,
-  cohort,
-  cw.gender,
-  cw.age,
-  cw.race,
-  cw.name_prefix,
-  cw.first_name,
-  cw.middle_name,
-  cw.last_name,
-  cw.name_suffix,
-  cw.registration_address_line_1,
-  cw.registration_address_line_2,
-  cw.registration_address_city,
-  cw.registration_address_state,
-  cw.registration_address_zip,
-  cw.registration_address_zip_plus_4,
-  cw.mail_address_line_1,
-  cw.mail_address_line_2,
-  cw.mail_address_city,
-  cw.mail_address_state,
-  cw.mail_address_zip,
-  cw.mail_address_zip_plus_4,
-  likely_cell,
-  likely_landline,
-  adopted_at,
-  confirmed_prepped_at,
-  confirmed_sent_at,
-  pledge_made_at
-FROM experiment_voter ev
-LEFT JOIN voters v ON ev.voter_id=v.registration_id
-JOIN catalist_raw cw on ev.voter_id=cw.dwid
-WHERE experiment_id IN ('1', '10')
-TO '/users/scott/Projects/votefwd/results/oh12_universe.csv' WITH CSV DELIMITER ',';
+\copy (SELECT voter_id as dwid, cohort, cw.gender, cw.age, cw.race, cw.name_prefix, cw.first_name, cw.middle_name, cw.last_name, cw.name_suffix, cw.registration_address_line_1, cw.registration_address_line_2, cw.registration_address_city, cw.registration_address_state, cw.registration_address_zip, cw.registration_address_zip_plus_4, cw.mail_address_line_1, cw.mail_address_line_2, cw.mail_address_city, cw.mail_address_state, cw.mail_address_zip, cw.mail_address_zip_plus_4, likely_cell, likely_landline, adopted_at, confirmed_prepped_at, confirmed_sent_at, pledge_made_at FROM experiment_voter ev LEFT JOIN voters v ON ev.voter_id=v.registration_id JOIN catalist_raw cw on ev.voter_id=cw.dwid WHERE experiment_id IN ('1','10')) TO '/users/scott/Projects/votefwd/results/oh12_universe.csv' WITH CSV DELIMITER ',';
+
+\copy (select voter_id as dwid, experiment_id, cohort, created_at, updated_at from experiment_voter where experiment_id='10') TO '/users/scott/Projects/votefwd/results/oh12b_universe.csv' with CSV DELIMITER ',';
