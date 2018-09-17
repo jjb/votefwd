c86efddfa1731f958fdb7f1a1d57f3fd584734e4
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2018-09-16 22:27:47 -0700

    Fix test description.

--- test/targeting-test.js
+++ test/targeting-test.js
@@ -7,7 +7,7 @@ var targeting = require('../server/targeting.js');
 describe('targeting', function() {
   describe('SelectVoters', function() {
 
-    it('should run the function in test env', function() {
+    it('should randomly assign  voters', function() {
       var dwids = Array.from({length: 10}, (x,i) => i);
       var experiment_voters = targeting.SelectVoters(dwids, 'MN2', 0.1)
       var cohorts = experiment_voters.map(row => row['cohort'])
