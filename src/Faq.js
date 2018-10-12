ab2e44290ee68439fe12018b9bd27b04e10a9a0c
Scott Forman <sjforman@gmail.com>
2018-10-11 22:04:59 -0700

    Updates FAQs

--- src/Faq.js
+++ src/Faq.js
@@ -12,7 +12,7 @@ class QsAndAs extends Component {
 
           <div className="mb-5">
             <h1>Frequently Asked Questions </h1>
-            <p>If you have a question not addressed here, <a href="https://bit.ly/votefwd-contact">contact us</a>.</p>
+            <p>If you have a question not addressed here, please send an email to <a href="mailto:help@votefwd.org">help@votefwd.org</a>.</p>
           </div>
 
           <div className="mb-5">
@@ -39,7 +39,7 @@ class QsAndAs extends Component {
           <div className="mb-5">
             <h2>How long does it take to get approved?</h2>
             <p>
-              We review new volunteers twice each day, so on average, you can expect to be approved within about 12 hours of completing the signup process. Please be patient, we’re welcoming you all as fast as we can!
+              We typically review new volunteers once each day, so on average, you can expect to be approved within about 12 hours of completing the signup process. Please be patient, we’re welcoming you all as fast as we can!
             </p>
           </div>
 
@@ -51,9 +51,9 @@ class QsAndAs extends Component {
           </div>
 
           <div className="mb-5">
-            <h2>How is this different from #PostcardsToVoters?</h2>
+            <h2>Can I write postcards instead of letters?</h2>
             <p>
-              Vote Forward letters are only partially handwritten, so they’re very fast to prepare (~90 seconds per letter). They are also evidently significantly more effective than postcards. The evidence we’ve seen suggests that postcards can yield a roughly 0.7 percentage point increase in turnout. By contrast, letters have been shown to increase turnout by as much as 3.4 percentage points. We hypothesize that recipients are more likely to take them seriously, because they’re a bit more formal.
+             We ask that all volunteers use the template and prepare the letters as the instructions describe. Our experimental evidence suggests that letters are significantly more effective than postcards, so we’re confident this is the right approach. Also, all Vote Forward campaigns are also randomized experiments; keeping the format consistent ensures the integrity of those experiments, so we can accurately assess the impact.
             </p>
           </div>
 
@@ -67,14 +67,14 @@ class QsAndAs extends Component {
           <div className="mb-5">
             <h2>How did you choose the districts?</h2>
             <p>
-              Starting from <a href="https://swingleft.org?utm_source=votefwd" target="_blank" rel="noopener noreferrer">Swing Left</a>’s list of 78 target districts, we’re focusing on the ones the <a href="https://projects.fivethirtyeight.com/2018-midterm-election-forecast/house?utm_source=votefwd" target="_blank" rel="noopener noreferrer">538 congressional model</a> projects will be closest. These are the districts most likely to determine control of the House. We also prioritize districts in states with important senate or gubernatorial races, and districts where we connections on the ground who can help kickstart volunteer efforts.
+              Starting from <a href="https://swingleft.org?utm_source=votefwd" target="_blank" rel="noopener noreferrer">Swing Left</a>’s list of 78 target districts, we selected districts that the <a href="https://projects.fivethirtyeight.com/2018-midterm-election-forecast/house?utm_source=votefwd" target="_blank" rel="noopener noreferrer">538 congressional model</a> projected in mid-August 2018 would be closest. These are the districts most likely to determine control of the House. We also prioritize districts in states with important Senate and gubernatorial races.
             </p>
           </div>
 
           <div className="mb-5">
-            <h2>Do I have to use my full name and return address?</h2>
+            <h2>Do I have to use my full name and home address?</h2>
             <p>
-              No! If you prefer not to sign your full name, you can use your first name and last initial. All letters are sent with an in-district return address, a UPS store mailbox. This way, after the election, we can tell which letters were not delivered.
+              No! If you prefer not to sign your full name, you can use your first name and last initial. Also, we provide an in-state address, a UPS store mailbox, to use as the return address. This way, volunteers don’t have to reveal their addresses. By centralizing the returns, we can also determine after the election which letters were not delivered.
             </p>
           </div>
 
@@ -86,9 +86,9 @@ class QsAndAs extends Component {
           </div>
 
           <div className="mb-5">
-            <h2>Where does the data come from?</h2>
+            <h2>Where do the addresses come from?</h2>
             <p>
-              Ultimately the data we’re using originates with state elections boards. They make voter rolls and histories available to the public in the interest of electoral system transparency, and to facilitate campaigning by candidates and independent organizations like Vote Forward. In most cases, we purchase data from <a href="https://www.catalist.us/" target="_blank" rel="noopener noreferrer">Catalist</a>, which standardizes the data the states provide into a unified format. Our vote propensity and partisanship predictions also come from Catalist.
+              Ultimately all the addresses originate with state election boards. They make voter rolls and histories available to the public in the interest of electoral system transparency, and to facilitate campaigning by candidates and organizations like Vote Forward. In most cases, we purchase data from <a href="https://www.catalist.us/" target="_blank" rel="noopener noreferrer">Catalist</a>, which standardizes the data into a unified format. Catalist also provides vote propensity and partisanship predictions.
             </p>
           </div>
 
