872f807400c74fb01a26f34e047efb6eaedc3b1a
Scott Forman <sjforman@gmail.com>
2018-10-12 12:21:38 -0700

    Adds a hello-world cron job.

--- server/slackService.js
+++ server/slackService.js
@@ -3,6 +3,11 @@
 
 var SlackUrl = process.env.REACT_APP_SLACK_WEBHOOK_URL;
 var slack = require('slack-notify')(SlackUrl);
+var cron = require('node-cron');
+
+cron.schedule('0 * * * *', () => {
+  console.log('running a task every hour');
+});
 
 function publishToSlack(message) {
   slack.send({
