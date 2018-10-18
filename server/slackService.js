7cf371009f5dff074046a0f5d8e18b7e433185f6
Mona Temchin <monatemchin@gmail.com>
2018-10-18 18:02:46 -0400

    Installed node-cron, changed interval on 'Hello World' cron job to every 30 minutes

--- server/slackService.js
+++ server/slackService.js
@@ -3,6 +3,13 @@
 
 var SlackUrl = process.env.REACT_APP_SLACK_WEBHOOK_URL;
 var slack = require('slack-notify')(SlackUrl);
+var cron = require('node-cron');
+
+cron.schedule('30 * * * *', () => {
+  console.log('running a task every half hour');
+  
+});
+
 
 function publishToSlack(message) {
   slack.send({
