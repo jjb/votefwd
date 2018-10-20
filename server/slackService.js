633267c3fd0a26e5ec90a25f933ee9b07de52db1
Mona Temchin <monatemchin@gmail.com>
2018-10-20 15:55:05 -0400

    Fleshed out nbrNewUsers function and set up to test it

--- server/slackService.js
+++ server/slackService.js
@@ -6,16 +6,18 @@ var SlackUrl = process.env.REACT_APP_SLACK_WEBHOOK_URL;
 var slack = require('slack-notify')(SlackUrl);
 var cron = require('node-cron');
 var db = require('./db');
-var n = 30;
+// var n = 30;
+var n = 1;
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + (today.getMinutes() - n) + ":" + today.getSeconds()
 var dateTime = date+' '+time;
 
-cron.schedule('*/n * * * *', () => {
-  console.log('running a task every thirty minutes');
-  publishToSlack('You are old, Father William, the Young Man said');
-
+// cron.schedule('*/n * * * *', () => {
+//   console.log('running a task every thirty minutes');
+  console.log('running a task every minute');
+  // publishToSlack('You are old, Father William, the Young Man said');
+  nbrNewUsers(n, dateTime, publishToSlack);
 });
 
 
@@ -26,12 +28,13 @@ function publishToSlack(message) {
   })
 }
 
-function nbrNewUsers(n, dateTime) {
+function nbrNewUsers(n, dateTime, callback) {
   db('users')
   .where({created_at >= dateTime})
-  .then(function(user){
-
-      return;
+  .then(function(){
+        var msg = 'the number of new users is ' + count(*);
+        callback(msg);
+        return;
     })
     .catch(err => {
         console.error(err);
