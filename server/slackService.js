cb2c1b65dc3f771541324775069c3b2ed96a39a1
Mona Temchin <monatemchin@gmail.com>
2018-10-20 17:37:49 -0400

    Modified nbrNewUsers function.  It now posts to Slack, but nbr is undefined

--- server/slackService.js
+++ server/slackService.js
@@ -7,15 +7,15 @@ var slack = require('slack-notify')(SlackUrl);
 var cron = require('node-cron');
 var db = require('./db');
 // var n = 30;
-var n = 1;
+var n = 2;
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + (today.getMinutes() - n) + ":" + today.getSeconds()
 var dateTime = date+' '+time;
 
-// cron.schedule('*/n * * * *', () => {
+cron.schedule('*/n * * * *', () => {
 //   console.log('running a task every thirty minutes');
-  console.log('running a task every minute');
+  console.log('running a task every two minutes');
   // publishToSlack('You are old, Father William, the Young Man said');
   nbrNewUsers(n, dateTime, publishToSlack);
 });
@@ -30,20 +30,25 @@ function publishToSlack(message) {
 
 function nbrNewUsers(n, dateTime, callback) {
   db('users')
-  .where({created_at >= dateTime})
-  .then(function(){
-        var msg = 'the number of new users is ' + count(*);
-        callback(msg);
+    .count()
+    .where('created_at' >= dateTime)
+    .then(function(results) {
+      let newUserCount = results.count;
+      if (newUserCount === 0) {
+        console.error(`no new users joined in last ${n} minutes.`);
+        callback(`no new users joined in last ${n} minutes.`);
         return;
+      } else {
+        callback(`number of new users in the last ${n} minutes is ${newUserCount}`);
+        return;
+      }
     })
     .catch(err => {
-        console.error(err);
-    });
-  })
-  .catch(err => {
       console.error(err);
-  });
+      callback(err);
+    });
 }
+
 
 module.exports = {
   publishToSlack
