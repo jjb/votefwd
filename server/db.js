527a94638a8fd8890a3d5acd56ddcf56f40bafcf
Scott Forman <sjforman@gmail.com>
2018-10-27 14:19:05 -0700

    Boost pool size to 100.

--- server/db.js
+++ server/db.js
@@ -18,7 +18,7 @@ if (process.env.REACT_APP_INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === '
 var knexClient = knex({
   client: process.env.REACT_APP_DATABASE_DIALECT,
   connection: config,
-  pool: { min: 2, max: 20 },
+  pool: { min: 2, max: 100 },
   migrations: {
     tableName: 'knex_migrations'
   }
