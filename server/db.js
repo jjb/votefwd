60b134fe465745aa2513b645612198ff6b1b107d
Scott Forman <sjforman@gmail.com>
2018-10-27 14:17:47 -0700

    Increases connection pool size to 20.

--- server/db.js
+++ server/db.js
@@ -18,6 +18,7 @@ if (process.env.REACT_APP_INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === '
 var knexClient = knex({
   client: process.env.REACT_APP_DATABASE_DIALECT,
   connection: config,
+  pool: { min: 2, max: 20 },
   migrations: {
     tableName: 'knex_migrations'
   }
